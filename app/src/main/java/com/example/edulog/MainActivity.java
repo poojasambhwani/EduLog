5796f7d9a6fd4ca5675d5ca1114c5b6b821eb4ab
Pooja Sambhwani <sambhwanipooja08@gmail.com>
2021-02-16 10:50:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/edulog/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.edulog;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private Button button1, button2;
+    LinearLayout sliderDotspanel;
+    Timer timer;
+    private int dotscount;
+    private ImageView[] dots;
+    private SliderPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView()
+                    .setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        setContentView(R.layout.activity_main);
+        // hide action bar you can use NoAction theme as well
+        getSupportActionBar().hide();
+        // bind views
+        viewPager = findViewById(R.id.pagerIntroSlider);
+        sliderDotspanel = (LinearLayout) findViewById(R.id.SliderDots);
+        button1 = findViewById(R.id.signin);
+        button2 = findViewById(R.id.signup);
+
+        adapter = new SliderPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        // set adapter
+        viewPager.setAdapter(adapter);
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new RemindTask(), 0, 2000);
+        // set dot indicators
+        // make status bar transparent
+
+        dotscount = adapter.getCount();
+        dots = new ImageView[dotscount];
+
+        for (int i = 0; i < dotscount; i++) {
+
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.slider_unselected_dot));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            params.setMargins(8, 0, 8, 0);
+
+            sliderDotspanel.addView(dots[i], params);
+
+        }
+
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.slider_selected_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                for (int i = 0; i < dotscount; i++) {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.slider_unselected_dot));
+                }
+
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.slider_selected_dot));
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+
+    class RemindTask extends TimerTask {
+        @Override
+        public void run() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (dotscount > 2) {
+                        dotscount = 0;
+                    } else {
+                        viewPager.setCurrentItem(dotscount++);
+                    }
+                }
+            });
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/edulog/SliderItemFragment.java
@@ -0,0 +1,67 @@
+package com.example.edulog;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SliderItemFragment extends Fragment {
+
+    private static final String ARG_POSITION = "slider-position";
+    private int position;
+    private Integer [] images = {R.drawable.ic_graduation,R.drawable.ic_online_class,R.drawable.ic_students};
+    private Integer [] texts = {R.string.online,R.string.tests,R.string.placement};
+    private Integer [] sub_texts = {R.string.engineering,R.string.access,R.string.widerange};
+
+    public SliderItemFragment() {
+
+    }
+
+    public static SliderItemFragment newInstance(int position) {
+        SliderItemFragment fragment = new SliderItemFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_POSITION, position);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            position = getArguments().getInt(ARG_POSITION);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_slider_item, container, false);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP) @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView title = view.findViewById(R.id.textView2);
+        TextView titleText1 = view.findViewById(R.id.textView3);
+        ImageView imageView = view.findViewById(R.id.imageView);
+        // set page title
+        title.setText(texts[position]);
+        // set page sub title text
+        titleText1.setText(sub_texts[position]);
+
+        // set page image
+        imageView.setImageResource(images[position]);
+    }
+}
